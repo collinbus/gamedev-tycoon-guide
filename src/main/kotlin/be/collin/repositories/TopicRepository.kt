af881505741dbbce96b886595e1abf1ccbc73041
collin <collin.busschots4@gmail.com>
2019-06-08 19:04:52 +0200

    add test shouldLoadCorrectTopics_When_GetTopicsInvoked

--- src/main/kotlin/be/collin/repositories/TopicRepository.kt
+++ src/main/kotlin/be/collin/repositories/TopicRepository.kt
@@ -1,9 +1,15 @@
 package be.collin.repositories
 
+import be.collin.domain.AudienceRatings
+import be.collin.domain.GenreRatings
+import be.collin.domain.Rating
 import be.collin.domain.Topic
 import java.util.*
 
 class TopicRepository {
+
+    private val ratings = Rating.values().associateBy(Rating::score)
+
     fun getTopics(): List<Topic> {
         val topics = mutableListOf<Topic>()
         val scanner = Scanner(javaClass.classLoader.getResourceAsStream("genre-test.csv"))
@@ -16,6 +22,29 @@ class TopicRepository {
     }
 
     private fun readTopic(line: String): Topic {
-        return Topic()
+        val fields = line.split(";")
+        val name = fields[0]
+        val genreRatings = readGenreRatings(fields)
+        val audienceRatings = readAudienceRatings(fields)
+        return Topic(name, genreRatings, audienceRatings)
+    }
+
+    private fun readGenreRatings(fields: List<String>): GenreRatings {
+        return GenreRatings(
+                ratings.getValue(fields[1].toInt()),
+                ratings.getValue(fields[2].toInt()),
+                ratings.getValue(fields[3].toInt()),
+                ratings.getValue(fields[4].toInt()),
+                ratings.getValue(fields[5].toInt()),
+                ratings.getValue(fields[6].toInt())
+        )
+    }
+
+    private fun readAudienceRatings(fields: List<String>): AudienceRatings {
+        return AudienceRatings(
+                ratings.getValue(fields[7].toInt()),
+                ratings.getValue(fields[8].toInt()),
+                ratings.getValue(fields[9].toInt())
+        )
     }
 }
--- src/test/kotlin/be/collin/repositories/TopicRepositoryTest.kt
+++ src/test/kotlin/be/collin/repositories/TopicRepositoryTest.kt
@@ -1,16 +1,54 @@
 package be.collin.repositories
 
+import be.collin.domain.AudienceRatings
+import be.collin.domain.GenreRatings
+import be.collin.domain.Rating
+import be.collin.domain.Topic
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
 class TopicRepositoryTest {
 
+    companion object {
+        val AIRPLANE_TOPIC = Topic("Airplane",
+                genreRatings = GenreRatings(Rating.BEST, Rating.WORST,Rating.DONT_CARE,
+                        Rating.BEST, Rating.BEST, Rating.BEST),
+                audienceRatings = AudienceRatings(Rating.BEST, Rating.BEST, Rating.BETTER))
+        val MUSIC_TOPIC = Topic("Music",
+                genreRatings = GenreRatings(Rating.BEST, Rating.BETTER, Rating.WORST,
+                        Rating.BEST, Rating.WORST, Rating.BEST),
+                audienceRatings = AudienceRatings(Rating.BEST, Rating.BETTER, Rating.GOOD))
+        val EVOLUTION_TOPIC = Topic("Evolution",
+                genreRatings = GenreRatings(Rating.WORSE, Rating.WORST, Rating.WORST,
+                        Rating.BEST, Rating.BEST, Rating.WORST),
+                audienceRatings = AudienceRatings(Rating.GOOD, Rating.BEST, Rating.WORSE))
+    }
+
+    private lateinit var topicRepository: TopicRepository
+
+    @Before
+    fun setUp() {
+        topicRepository = TopicRepository()
+    }
+
     @Test
-    fun shouldLoadTopics_When_GetTopicsInvoked() {
-        val repository = TopicRepository()
+    fun shouldLoad3Topics_When_GetTopicsInvoked() {
+        val repository = topicRepository
 
         val topics = repository.getTopics()
 
         assertEquals(3,topics.size)
+    }
+
+    @Test
+    fun shouldLoadCorrectTopics_When_GetTopicsInvoked() {
+        val repository = topicRepository
+
+        val topics = repository.getTopics()
+
+        assertEquals(AIRPLANE_TOPIC,topics[0])
+        assertEquals(MUSIC_TOPIC, topics[1])
+        assertEquals(EVOLUTION_TOPIC, topics[2])
     }
 }
