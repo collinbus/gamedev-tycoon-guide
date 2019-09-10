d4abe29ad1f39dfa6f557340c00c35f07d60e74d
collin <collin.busschots4@gmail.com>
2019-09-10 21:11:58 +0200

    extracted proposal box to seperate component

---
+++ src/main/kotlin/be/collin/views/ProposalBox.kt
@@ -0,0 +1,22 @@
+package be.collin.views
+
+import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
+import javafx.scene.layout.VBox
+import javafx.scene.text.Text
+
+
+class ProposalBox() : VBox() {
+
+    @FXML
+    private lateinit var title: Text
+
+    init {
+        val fxmlLoader = FXMLLoader(javaClass.classLoader.getResource("be/collin/views/proposal_box.fxml"))
+        fxmlLoader.setRoot(this)
+        fxmlLoader.setController(this)
+        fxmlLoader.load<Any>()
+
+        title.text = "Next Game:"
+    }
+}
