2970efdc8d8e5ce3e17ac44c3b98c7e103bef780
collin <collin.busschots4@gmail.com>
2019-10-27 18:04:23 +0100

    WIP prepare for calculating scores

--- src/main/kotlin/be/collin/domain/Game.kt
+++ src/main/kotlin/be/collin/domain/Game.kt
@@ -2,4 +2,5 @@ package be.collin.domain
 
 data class Game(val topic: GenreAudienceItem,
                 val system: GenreAudienceItem,
-                val genreRatings: GenreScore)
+                val genre: Genre,
+                val score: Int)
--- src/main/kotlin/be/collin/services/ScoreCalculator.kt
+++ src/main/kotlin/be/collin/services/ScoreCalculator.kt
@@ -1,15 +1,39 @@
 package be.collin.services
 
-import be.collin.domain.Game
-import be.collin.domain.Genre
-import be.collin.domain.GenreLayout
-import be.collin.domain.GenreAudienceItem
-import java.util.*
+import be.collin.domain.*
 
 class ScoreCalculator {
 
-    fun getBestScores(topics: List<GenreAudienceItem>, systems: List<GenreAudienceItem>): MutableSet<Game> {
-        val games = mutableSetOf<Game>()
+    fun getBestScores(topics: List<GenreAudienceItem>, systems: List<GenreAudienceItem>): MutableList<Game> {
+        val games = mutableListOf<Game>()
+        val topScorePerSystem = mutableMapOf<GenreAudienceItem, List<Pair<Genre, Rating>>>()
+        val topScorePerTopic = mutableMapOf<GenreAudienceItem, List<Pair<Genre, Rating>>>()
+
+        systems.forEach { system ->
+            topScorePerSystem[system] = sortGenres(system.genreRatings)
+        }
+
+        topics.forEach { topic ->
+            topScorePerTopic[topic] = sortGenres(topic.genreRatings)
+        }
+
+        topScorePerSystem.keys.forEach { system ->
+            topScorePerTopic.keys.forEach { topic ->
+                val systemScores = topScorePerSystem[system]!!
+                val topicScores = topScorePerTopic[topic]!!
+                //TODO get corresponding genre per system and topic
+            }
+        }
+
         return games
+    }
+
+    private fun sortGenres(genreRatings: GenreRatings): List<Pair<Genre, Rating>> {
+        val genres = mutableListOf(Pair(Genre.ACTION, genreRatings.action), Pair(Genre.RPG, genreRatings.rpg),
+                Pair(Genre.ADVENTURE, genreRatings.adventure), Pair(Genre.CASUAL, genreRatings.casual),
+                Pair(Genre.SIMULATION, genreRatings.simulation), Pair(Genre.STRATEGY, genreRatings.strategy))
+
+        genres.sortByDescending { pair -> pair.second.score }
+        return genres.toList()
     }
 }
