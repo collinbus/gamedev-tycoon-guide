e4e9b7585aaec5f3ceb68e8a7641c5264375ac81
collin <collin.busschots4@gmail.com>
2019-12-08 15:10:43 +0100

    Add games to listview when generate button is clicked

--- src/main/kotlin/be/collin/controllers/GenerateGameController.kt
+++ src/main/kotlin/be/collin/controllers/GenerateGameController.kt
@@ -1,15 +1,23 @@
 package be.collin.controllers
 
+import be.collin.domain.Game
 import be.collin.domain.GenreAudienceItem
+import be.collin.services.GameService
 import be.collin.services.SimpleScoreCalculator
+import javafx.collections.FXCollections
+import javafx.fxml.FXML
+import javafx.scene.control.ListView
 
 class GenerateGameController(private val systems: List<GenreAudienceItem>,
                              private val topics: List<GenreAudienceItem>) {
 
+    @FXML
+    lateinit var games: ListView<Game>
 
-    private val scoreCalculator = SimpleScoreCalculator()
+    private val gameService = GameService(SimpleScoreCalculator())
 
     fun generateGame() {
-        scoreCalculator.calculateScores(systems, topics)
+        val top5Games = gameService.getTop5Games(topics, systems)
+        games.items = FXCollections.observableArrayList(top5Games)
     }
 }
