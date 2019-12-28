d021ea758172b0c82d194c2419057fecdfeb574b
Collin <collin.busschots4@gmail.com>
2019-12-29 00:56:54 +0100

    Remove unused class

--- src/main/kotlin/be/collin/domain/GenreScore.kt
+++
@@ -1,9 +0,0 @@
-package be.collin.domain
-
-class GenreScore(val action: Int,
-                 val adventure: Int,
-                 val rpg: Int,
-                 val simulation: Int,
-                 val strategy: Int,
-                 val casual: Int) {
-}
